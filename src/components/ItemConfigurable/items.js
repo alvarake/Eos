2ae0e133bb1b7989d0dd9d47ad8130bd599e2d3d
Alvarake <alvarocepeda2@gmail.com>
2019-05-03 16:20:09 +0200

    itemsConfigurables separado de la lógica de APP

---
+++ src/components/ItemConfigurable/items.js
@@ -0,0 +1,10 @@
+let music = {type:'Music', path:'/welcome/settings/music', url_img:'music.jpg'};
+let alarm = {type:'Alarm', path:'/welcome/settings/alarm', url_img:'clock.jpg'};
+let route = {type:'Route', path:'/welcome/settings/route', url_img:'bus.jpeg'};
+let weather = {type:'Weather', path:'/welcome/settings/weather', url_img:'weather.jpg'};
+let news = {type:'News', path:'/welcome/settings/news', url_img:'newspaper.jpg'};
+
+
+const items= [music, alarm, route, weather, news];
+
+export default items;
