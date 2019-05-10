52088722c4d960da64281108f54139aa6bbcd554
Alvarake <alvarocepeda2@gmail.com>
2019-05-10 15:38:46 +0200

    Vamos a ver

--- src/containers/AppStateDecorator.jsx
+++ src/containers/AppStateDecorator.jsx
@@ -1,14 +1,20 @@
 import {connect} from 'react-redux';
 import {navigate} from '../actions/RouterActions';
-import {set_music_settings, set_alarm_settings} from '../actions/SettingsActions';
+import {set_music_settings} from '../actions/SettingsActions';
+import {notification_createAlert} from '../actions/AlarmActions';
 
 const mapStateToProps = (state) => {
 	console.log(state)
 	return {
 		path: state.path,
 		settings: {
-			music : state.settings.music,
-			alarm: state.settings.alarm
+			music: state.settings.music,
+			alarm: {
+				notification: {
+					alertId: state.settings.alarm.notification.alertId,
+					returnValue: state.settings.alarm.notification.returnValue
+				}
+			}
 		}
 	}
 };
@@ -17,7 +23,7 @@ const mapDispatchToProps = (dispatch) => {
 	return {
 		onNavigate: ({path}) => dispatch(navigate(path)),
 		onMusicSettings: ({settings}) => dispatch(set_music_settings(settings)),
-		onAlarmSettings: ({settings}) => dispatch(set_alarm_settings(settings))
+		onAlarmSettings: ({params}) => dispatch(notification_createAlert(params))
 	}
 };
 
