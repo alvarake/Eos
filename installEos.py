5464405a35919bfa25e764c27521b29bf5bdfd6c
Alvarake <alvarocepeda2@gmail.com>
2019-05-19 15:47:57 +0200

    Comunicación conseguida con servicio propio; Script de empaquetar e instalar modificada para incluir servicios

--- installEos.py
+++ installEos.py
@@ -1,14 +1,38 @@
 import os
 
+print("###################################################")
+
 print("Borrando paquetes anteriores")
-os.system("rm -rf eos_0.0.1_all.ipk dist/")
+print("Ejecutando: 'cd app; npm run clean'")
+os.system("cd app; npm run clean")
+
+print("###################################################")
+
 print("Haciendo Paquete Enact")
-os.system("enact pack")
-print("Empaquetando para la Raspberry")
-os.system("ares-package dist")
-print("Instalando Eos en la Raspberry")
-os.system("ares-install --device RaspAlvarake eos_0.0.1_all.ipk")
-print("Abriendo Depurador")
-os.system("ares-inspect --device RaspAlvarake --app eos --open")
+print("Ejecutando: 'cd app; enact pack'")
+os.system("cd app; enact pack")
+
+print("###################################################")
+try:
+    
+    print("Empaquetando para la Raspberry")
+    print("Ejecutando: 'ares-package app/dist service'")
+    os.system("ares-package app/dist service")
+
+    print("###################################################")
+
+    print("Instalando Eos en la Raspberry")
+    print("Ejecutando: 'ares-install --device RaspAlvarake eos_0.0.1_all.ipk'")
+    os.system("ares-install --device RaspAlvarake eos_0.0.1_all.ipk")
+
+    print("###################################################")
+
+    print("Abriendo Depurador")
+    print("Ejecutando: 'ares-inspect --device RaspAlvarake --app eos --open'")
+    os.system("ares-inspect --device RaspAlvarake --app eos --open")
+
+except:
+    print("Ha habido un fallo con los comandos 'ares'")
+    print("Intenta ejecutar desde ~ 'source .profile' y prueba otra vez")
 
 
