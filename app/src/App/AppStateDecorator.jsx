473fcdf4de243f7b6d49f4fdd00b7f11e3b002a8
Alvarake <alvarocepeda2@gmail.com>
2019-05-26 12:20:41 +0200

    MusicSettings pasa eslint

--- app/src/App/AppStateDecorator.jsx
+++ app/src/App/AppStateDecorator.jsx
@@ -1,16 +1,16 @@
-import {connect} from 'react-redux';
-import {navigate} from '../actions/RouterActions';
-import {unload_Media, load_Media} from '../actions/MusicActions';
-import {calculateDeviceTime} from '../actions/AlarmActions';
+import { connect } from 'react-redux';
+import { navigate } from '../actions/RouterActions';
+import { unloadMedia, loadMedia } from '../actions/MusicActions';
+import { calculateDeviceTime } from '../actions/AlarmActions';
 
 
 const mapStateToProps = (state) => {
-	console.log(state)
+	console.log(state);
 	return {
 		path: state.path,
 		music: {
 			configured: state.music.configured,
-			sound: state.music.sound
+			sound: state.music.sound,
 		},
 		alarm: {
 			configured: state.alarm.configured,
@@ -18,21 +18,20 @@ const mapStateToProps = (state) => {
 			alarmtimestamp: state.alarm.alarmtimestamp,
 			notification: {
 				alertId: state.alarm.notification.alertId,
-				returnValue: state.alarm.notification.returnValue
-			}
-		}
-	}
-};
-//AQUI SOLO DEBERIAN CREARSE ACCIONES
-const mapDispatchToProps = (dispatch) => {
-	return {
-		onNavigate: ({path}) => dispatch(navigate(path)),
-		onMusicSettings: (music) => (music.configured ? dispatch(unload_Media(music.sound)) : dispatch(load_Media(music))),
-		onAlarmSettings: (alarmtime) =>dispatch(calculateDeviceTime(alarmtime))
-	}
+				returnValue: state.alarm.notification.returnValue,
+			},
+		},
+	};
 };
 
-	const AppStateDecorator = connect(mapStateToProps, mapDispatchToProps);
+const mapDispatchToProps = dispatch => ({
+	onNavigate: ({ path }) => dispatch(navigate(path)),
+	// eslint-disable-next-line max-len
+	onMusicSettings: music => (music.configured ? dispatch(unloadMedia(music.sound)) : dispatch(loadMedia())),
+	onAlarmSettings: alarmtime => dispatch(calculateDeviceTime(alarmtime)),
+});
 
-	export default AppStateDecorator;
-	export {AppStateDecorator};
+const AppStateDecorator = connect(mapStateToProps, mapDispatchToProps);
+
+export default AppStateDecorator;
+export { AppStateDecorator };
--- app/src/actions/MusicActions.js
+++ app/src/actions/MusicActions.js
@@ -1,90 +1,93 @@
 import LS2Request from '@enact/webos/LS2Request';
 
-export const set_music_settings = (music) => {
+export const setMusicSettings = (music) => {
+	console.log('En setMusicSettings');
 	return {
-		type: "SET_MUSIC_SETTINGS",
-		music
-	}
-}
+		type: 'SET_MUSIC_SETTINGS',
+		music,
+	};
+};
 
-export const play_Media = (mediaId)=> {
+export const playMedia = (mediaId) => {
+	console.log('En playMedia');
 	return new LS2Request().send({
 		service: 'luna://com.webos.media',
-		method:"play",
-		parameters:{
-			mediaId : mediaId
+		method: 'play',
+		parameters: {
+			mediaId,
 		},
 		onSuccess: (res) => {
-			console.log(res)
-			console.log(mediaId)
+			console.log(res);
 		},
 		onFailure: (res) => {
-			console.log(res)
+			console.log(res);
 		},
 	});
 };
 
-export const pause_Media = (mediaId) => {
+export const pauseMedia = (mediaId) => {
+	console.log('En pauseMedia');
 	return new LS2Request().send({
 		service: 'luna://com.webos.media',
 		method: 'pause',
-		parameters:{
-			mediaId : mediaId
+		parameters: {
+			mediaId,
 		},
 		onSuccess: (res) => {
-			console.log("Se ha pausado el archivo")
-			console.log(res)
+			console.log('Se ha pausado el archivo');
+			console.log(res);
 		},
 		onFailure: (res) => {
-			console.log(res)
+			console.log(res);
 		},
 	});
 };
 
-export const unload_Media = (mediaId) => dispatch => {
+export const unloadMedia = mediaId => (dispatch) => {
+	console.log('En unloadMedia');
 	return new LS2Request().send({
 		service: 'luna://com.webos.media',
 		method: 'unload',
-		parameters:{
-			mediaId : mediaId
+		parameters: {
+			mediaId,
 		},
 		onSuccess: (res) => {
-			console.log("Se ha terminado de des-cargar el archivo")
-			console.log(res)
-			const music ={sound: "", configured:false};
-			dispatch(set_music_settings(music));
+			console.log('Se ha terminado de des-cargar el archivo');
+			console.log(res);
+			const music = { sound: '', configured: false };
+			dispatch(setMusicSettings(music));
 		},
 		onFailure: (res) => {
-			console.log(res)
+			console.log(res);
 		},
 	});
 };
 
-export const load_Media = music => dispatch => {
+export const loadMedia = () => (dispatch) => {
+	console.log('En loadMedia');
+	console.log(dispatch);
 	return new LS2Request().send({
 		service: 'luna://com.webos.media',
 		method: 'load',
-		parameters:{
+		parameters: {
 			uri: 'https://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.mp3',
-			type :'media',
+			type: 'media',
 			payload: {
 				option: {
-					appId: "eos"
-				}
-			}
+					appId: 'eos',
+				},
+			},
 		},
 		onSuccess: (res) => {
-			console.log("Se ha terminado de cargar el archivo");
+			console.log('Se ha terminado de cargar el archivo');
 			console.log(res);
-			music ={sound: res.mediaId, configured:true};
-			dispatch(set_music_settings(music));
+			const musicConfingSucces = { sound: res.mediaId, configured: true };
+			dispatch(setMusicSettings(musicConfingSucces));
 		},
 		onFailure: (res) => {
-			console.log(res)
-			music ={sound: "", configured:false};
-			dispatch(set_music_settings(music));
-
+			console.log(res);
+			const musicConfingFailure = { sound: '', configured: false };
+			dispatch(setMusicSettings(musicConfingFailure));
 		},
 	});
 };
-
