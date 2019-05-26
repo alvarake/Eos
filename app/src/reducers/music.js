214bba69498178c0b3e7fc5b2e6cf65bfcf8a2ca
Alvarake <alvarocepeda2@gmail.com>
2019-05-26 12:20:41 +0200

    MusicSettings pasa eslint

--- app/src/reducers/music.js
+++ app/src/reducers/music.js
@@ -1,19 +1,18 @@
-const initialState ={
-		configured: false,
-		sound: ''
-}
+const initialState = {
+	configured: false,
+	sound: '',
+};
 
-function music (state= initialState, action){
+function music(state = initialState, action) {
 	switch (action.type) {
-		case 'SET_MUSIC_SETTINGS':
-			//return action.music;
-			return {
-				...state,
-                configured: action.music.configured,
-                sound: action.music.sound
-			}
-		default:
-			return state;
+	case 'SET_MUSIC_SETTINGS':
+		return {
+			...state,
+			configured: action.music.configured,
+			sound: action.music.sound,
+		};
+	default:
+		return state;
 	}
 }
 
--- app/src/views/settingsViews/MusicSettingsPanel.js
+++ app/src/views/settingsViews/MusicSettingsPanel.js
@@ -1,9 +1,9 @@
 import Button from '@enact/moonstone/Button';
-import {Header, Panel} from '@enact/moonstone/Panels';
+import { Header, Panel } from '@enact/moonstone/Panels';
 import kind from '@enact/core/kind';
 import PropTypes from 'prop-types';
 import React from 'react';
-import {pause_Media, play_Media} from '../../actions/MusicActions';
+import { pauseMedia, playMedia } from '../../actions/MusicActions';
 
 const MusicSettingsPanel = kind({
 	name: 'MusicSettingsPanel',
@@ -12,21 +12,21 @@ const MusicSettingsPanel = kind({
 		music: PropTypes.object,
 		onClick: PropTypes.func,
 		onSettings: PropTypes.func,
-		title: PropTypes.string
+		title: PropTypes.string,
 	},
 
-	handlers:{
-		pause: (ev, {music}) => pause_Media(music.sound),
-		play: (ev, {music}) => play_Media(music.sound)
+	handlers: {
+		pause: (ev, { music }) => pauseMedia(music.sound),
+		play: (ev, { music }) => playMedia(music.sound),
 	},
 
-	render: ({music, onClick, onSettings, pause, play, title, ...rest}) => {
+	render: ({ music, onClick, onSettings, pause, play, title, ...rest }) => {
 		return (
 			<Panel {...rest}>
 				<Header title={title}>
-				<Button onClick={onClick}>Atras</Button>
+					<Button onClick={onClick}>Atras</Button>
 				</Header>
-				<Button onClick={onSettings}>{music.configured ? "Borrar Cancion" : "Cargar Cancion" }</Button>
+				<Button onClick={onSettings}>{music.configured ? 'Borrar Cancion' : 'Cargar Cancion' }</Button>
 				<Button onClick={play}>Play</Button>
 				<Button onClick={pause}>Pausar</Button>
 			</Panel>
