bada67416127f5b70a747a7bceec3a6ea89cb92d
Alvarake <alvarocepeda2@gmail.com>
2019-05-28 17:54:23 +0200

    Cuanto le queda al bus añadido al servicio busservice

--- app/src/actions/BusActions.js
+++ app/src/actions/BusActions.js
@@ -1,7 +1,6 @@
 import LS2Request from '@enact/webos/LS2Request';
 
 const setBusSettings = (stopid) => {
-	console.log('En setBusSettings');
 	return {
 		type: 'SET_BUS_SETTINGS',
 		stopid,
@@ -9,22 +8,21 @@ const setBusSettings = (stopid) => {
 };
 
 const setEMTAccessToken = (accessToken) => {
-	console.log('En setEMTAccessToken');
-	console.log(accessToken);
 	return {
 		type: 'SET_EMT_ACCESSTOKEN',
 		accessToken,
 	};
 };
 
-export const loginEMT = () => (dispatch) => {
-	console.log('En loginEMT');
+export const loadStopInfo = ({ stopid, accessToken }) => (dispatch) => {
+	console.log('En loadStopInfo');
 	return new LS2Request().send({
 		service: 'luna://eos.busservice',
-		method: 'login',
+		method: 'stopdetail',
+		parameters: { stopid, accessToken },
 		onSuccess: (res) => {
-			console.log('Se ha pedido el tokken');
-			console.log(res);
+			console.log(res.data);
+			dispatch(setBusSettings(stopid));
 			dispatch(setEMTAccessToken(res.accessToken));
 		},
 		onFailure: (res) => {
@@ -34,18 +32,18 @@ export const loginEMT = () => (dispatch) => {
 };
 
 
-export const loadStopInfo = ({ stopid, accessToken }) => (dispatch) => {
-	console.log('En loadStopInfo');
+export const timeToArrive = ({ stopid, accessToken }) => {
+	console.log('En timeToArrive');
 	return new LS2Request().send({
 		service: 'luna://eos.busservice',
-		method: 'stopdetail',
+		method: 'arrivalbus',
 		parameters: { stopid, accessToken },
 		onSuccess: (res) => {
-			console.log(res);
-			dispatch(setBusSettings(res.stopid));
+			console.log(res.data);
 		},
 		onFailure: (res) => {
 			console.log(res);
 		},
 	});
 };
+
