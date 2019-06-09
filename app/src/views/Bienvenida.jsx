8b94ab8f75d066e609131c5ec0b29f7f684b7035
Alvarake <alvarocepeda2@gmail.com>
2019-06-09 17:45:46 +0200

    Mensaje Bienvenida mas grande

--- app/src/views/Bienvenida.jsx
+++ app/src/views/Bienvenida.jsx
@@ -1,4 +1,3 @@
-import BodyText from '@enact/moonstone/BodyText';
 import Button from '@enact/moonstone/Button';
 import kind from '@enact/core/kind';
 import { Panel, Header } from '@enact/moonstone/Panels';
@@ -15,24 +14,27 @@ const Bienvenida = kind({
 		title: PropTypes.string,
 	},
 
-	render: ({ title, onClick, ...rest }) => (
-		<Panel {...rest}>
-			<Header title={title}>
-				<Button onClick={onClick}>Configuración</Button>
-			</Header>
-			<Scroller>
-				<BodyText>
-		Hola, soy eOS tu asistente personal en la mañana. He venido para hacerte la vida más fácil.
-				</BodyText>
-				<BodyText>
-		Primero vamos a configurar algunas cosas que me ayudarán a ayudarte.
-				</BodyText>
-				<BodyText>
-		Recuerda:
-		¡A quién madruga eOS ayuda!
-				</BodyText>
-			</Scroller>
-		</Panel>
-	),
+	render: ({ title, onClick, ...rest }) => {
+		const h1Style = {
+			fontSize: 94,
+		};
+		const eOS = {
+			color: 'red',
+		};
+
+		return (
+			<Panel {...rest}>
+				<Header title={title}>
+					<Button onClick={onClick}>Configuración</Button>
+				</Header>
+				<Scroller>
+					<h1 style={h1Style}> Hola, soy <span style={eOS}>eOS</span>, tu asistente personal.</h1>
+					<h1 style={h1Style}>Estoy aqui para hacerte la vida más <span style={eOS}>fácil</span>.</h1>
+					<h1>Vamos a configurar algunas cosas que me ayudarán a ayudarte.</h1>
+					<h1 style={h1Style}>Recuerda: ¡A quién madruga <span style={eOS}>eOS</span> ayuda!</h1>
+				</Scroller>
+			</Panel>
+		);
+	},
 });
 export default Bienvenida;
